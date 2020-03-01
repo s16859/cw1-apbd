918a2330c0259a7674f2d81ea784151881380da4
s16859 <s16859@LAB214-04>
2020-03-01 14:27:47 +0100

    email regex

--- Ćwiczenia1/Ćwiczenia1/Program.cs
+++ Ćwiczenia1/Ćwiczenia1/Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Ćwiczenia1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+            }
+
+            
             Console.WriteLine("Hello World!");
         }
     }
