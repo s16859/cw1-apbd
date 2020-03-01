3b1ad2cb4b6a9781ced8f2eb704ca9bb8618397e
s16859 <s16859@LAB214-04>
2020-03-01 14:00:16 +0100

    nowy projekt

---
+++ Ćwiczenia1/Ćwiczenia1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ćwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
